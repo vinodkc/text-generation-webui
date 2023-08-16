1a4fa22e8bb6012f0efa365f7528cdccdbc85620
Vinod KC <vinod.kc.in@gmail.com>
2023-08-16 15:52:58 -0700

    actual requirement

--- setupscripts/1_session-install-deps/install_dependencies.py
+++ setupscripts/1_session-install-deps/install_dependencies.py
@@ -1,3 +1,4 @@
 print("install_dependencies.py")
 !pip install --upgrade pip setuptools==59.8.0 wheel
-!pip install --no-cache-dir --log pip-req.log -r setupscripts/1_session-install-deps/requirements.txt
+!pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+!pip install --no-cache-dir --log pip-req.log -r requirements.txt
