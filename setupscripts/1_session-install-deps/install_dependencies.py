96327eb64dd30f6bac2e4316b71e3167ec188133
Vinod KC <vinod.kc.in@gmail.com>
2023-08-16 12:43:38 -0700

    z

--- setupscripts/1_session-install-deps/install_dependencies.py
+++ setupscripts/1_session-install-deps/install_dependencies.py
@@ -1,3 +1,3 @@
 print("install_dependencies.py")
-!pip install --upgrade pip
-!pip install --no-cache-dir --log pip-req.log -r setupscripts/1_session-install-deps/requirements.txt
+!pip3 install --upgrade pip3
+!pip3 install --no-cache-dir --log pip-req.log -r setupscripts/1_session-install-deps/requirements.txt
