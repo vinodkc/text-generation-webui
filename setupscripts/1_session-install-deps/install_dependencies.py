f94f39f8cf918f3c96ab72acfd4780dc8ba996ea
Vinod KC <vinod.kc.in@gmail.com>
2023-08-16 13:58:40 -0700

    setuptool < 60

--- setupscripts/1_session-install-deps/install_dependencies.py
+++ setupscripts/1_session-install-deps/install_dependencies.py
@@ -1,3 +1,3 @@
 print("install_dependencies.py")
-!pip3 install --upgrade pip3
-!pip3 install --no-cache-dir --log pip-req.log -r setupscripts/1_session-install-deps/requirements.txt
+!pip install --upgrade pip setuptools==59.8.0 wheel
+!pip install --no-cache-dir --log pip-req.log -r setupscripts/1_session-install-deps/requirements.txt
